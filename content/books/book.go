17349b30ce79f458d55881bd14c379d2cdf95a56
jochen <jochen.czemmel@web.de>
2023-10-29 19:12:28 +0100

    add book unit tests

--- content/books/book.go
+++ content/books/book.go
@@ -1,50 +1,35 @@
 // Package books provides informaion about Books, Lessons and Content.
 package books
 
-type series struct {
-	seriesTitle string // the title of the book collection
+type Series struct {
+	SeriesTitle string // the title of the book collection
 	Books       []*Book
 }
 
 // Book represents a book with lessons. It is optionally
 // a volume of a series/collection of books.
 type Book struct {
-	title         string             // the title of the book
-	seriesTitle   string             // the title of the book collection
-	volume        int                // the volume number in the collection
+	Title         string             // the title of the book
+	SeriesTitle   string             // the title of the book collection
+	Volume        int                // the volume number in the collection
 	lessons       []string           // the ordered lessons titles
 	lessonByTitle map[string]*Lesson // the lessons by title
 }
 
 // New returns a new book with the given title.
-func New(title, seriestitle string) Book {
+func New(title, seriestitle string, lessons ...*Lesson) Book {
 	book := Book{
-		title:         title,
-		seriesTitle:   title,
+		Title:         title,
+		SeriesTitle:   title,
 		lessons:       []string{},
 		lessonByTitle: map[string]*Lesson{},
 	}
 	if seriestitle != "" {
-		book.seriesTitle = seriestitle
+		book.SeriesTitle = seriestitle
 	}
+	book.Add(lessons...)
 
 	return book
-}
-
-// Title returns the book title.
-func (b Book) Title() string {
-	return b.title
-}
-
-// SeriesTitle returns the title of the series
-// in which the book is contained.
-func (b Book) SeriesTitle() string {
-	return b.seriesTitle
-}
-
-// VolumeNumber returns the number of the book in the series.
-func (b Book) VolumeNumber() int {
-	return b.volume
 }
 
 // Lessons returns all lessons in the book in the
@@ -53,7 +38,7 @@ func (b Book) Lessons() []*Lesson {
 	if b.lessons == nil || b.lessonByTitle == nil {
 		return []*Lesson{}
 	}
-	result := make([]*Lesson, len(b.lessons))
+	result := make([]*Lesson, 0, len(b.lessons))
 	for _, title := range b.lessons {
 		result = append(result, b.lessonByTitle[title])
 	}
@@ -72,6 +57,7 @@ func (b *Book) Add(lessons ...*Lesson) {
 		_, ok := b.lessonByTitle[title]
 		if ok {
 			b.lessonByTitle[title] = lesson
+
 			continue
 		}
 		b.lessonByTitle[title] = lesson
@@ -79,17 +65,33 @@ func (b *Book) Add(lessons ...*Lesson) {
 	}
 }
 
-// SetTitle sets the book title.
-func (b *Book) SetTitle(title string) {
-	b.title = title
-}
-
-// SetSeriesTitle sets the book series title.
-func (b *Book) SetSeriesTitle(title string) {
-	b.seriesTitle = title
-}
-
-// SetVolume sets the book series volume.
-func (b *Book) SetVolume(volume int) {
-	b.volume = volume
-}
+// // SetTitle sets the book title.
+// func (b *Book) SetTitle(title string) {
+// 	b.title = title
+// }
+//
+// // SetSeriesTitle sets the book series title.
+// func (b *Book) SetSeriesTitle(title string) {
+// 	b.seriesTitle = title
+// }
+//
+// // SetVolume sets the book series volume.
+// func (b *Book) SetVolume(volume int) {
+// 	b.volume = volume
+// }
+//
+// // Title returns the book title.
+// func (b Book) Title() string {
+// 	return b.title
+// }
+//
+// // SeriesTitle returns the title of the series
+// // in which the book is contained.
+// func (b Book) SeriesTitle() string {
+// 	return b.seriesTitle
+// }
+//
+// // VolumeNumber returns the number of the book in the series.
+// func (b Book) VolumeNumber() int {
+// 	return b.volume
+// }
--- content/books/book_test.go
+++ content/books/book_test.go
@@ -3,111 +3,64 @@ package books_test
 import (
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
-	"github.com/jochenczemmel/gobenkyoo/content"
 	"github.com/jochenczemmel/gobenkyoo/content/books"
 )
 
-func TestBook(t *testing.T) {
+var series1 = "series1"
 
-	bookTitle := "book1"
-	lessons := []books.Lesson[DummyCard]{
-		books.NewLesson[DummyCard]("lesson1", ""),
-		books.NewLesson[DummyCard]("lesson2", ""),
-		books.NewLesson[DummyCard]("lesson3", ""),
-	}
-	wantLessonTitles := []string{
-		"lesson1",
-		"lesson2",
-		"lesson3",
-	}
+func TestBookGetters(t *testing.T) {
 
-	t.Run("empty book", func(t *testing.T) {
+	lesson1 := books.NewLesson("lesson1", book1)
+	lesson2 := books.NewLesson("lesson2", book1)
+	lesson3 := books.NewLesson("lesson3", book1)
 
-		book := books.New[DummyCard](bookTitle)
-		got := book.Title()
-		if got != bookTitle {
-			t.Errorf("ERROR: got %s, want %s", got, bookTitle)
-		}
-		gotLessons := book.Lessons()
-		want := 0
-		if got := len(gotLessons); got > want {
-			t.Errorf("ERROR: got %v, want %v", got, want)
-		}
-	})
+	var emptyBook books.Book
+	emptyBook.Add(&lesson1, &lesson2, &lesson3)
 
-	t.Run("initial content", func(t *testing.T) {
-		book := books.New(bookTitle, lessons...)
-		compareLessonTitles(t, book.Lessons(), wantLessonTitles)
-	})
-
-	t.Run("add content", func(t *testing.T) {
-		book := books.New[DummyCard](bookTitle)
-		book.AppendLesson(lessons...)
-		compareLessonTitles(t, book.Lessons(), wantLessonTitles)
-	})
-
-	t.Run("add duplicates", func(t *testing.T) {
-		book := books.New(bookTitle, lessons...)
-		book.AppendLesson(lessons...)
-		compareLessonTitles(t, book.Lessons(), wantLessonTitles)
-	})
-
-	t.Run("add more content", func(t *testing.T) {
-		book := books.New(bookTitle, lessons...)
-		book.AppendLesson(
-			books.NewLesson[DummyCard]("lesson4", bookTitle),
-		)
-		compareLessonTitles(t, book.Lessons(),
-			append(wantLessonTitles, "lesson4"),
-		)
-	})
-
-	t.Run("get lesson by title", func(t *testing.T) {
-		book := books.New(bookTitle, lessons...)
-		cases := []struct {
-			name          string
-			title         string
-			want          bool
-			wantBookTitle string
-		}{
-			{
-				name:          "found",
-				title:         "lesson2",
-				want:          true,
-				wantBookTitle: bookTitle,
-			},
-			{
-				name:          "not found",
-				title:         "not found",
-				want:          false,
-				wantBookTitle: "",
-			},
-		}
-
-		for _, c := range cases {
-			t.Run(c.name, func(t *testing.T) {
-				foundLesson, got := book.Lesson(c.title)
-				if got != c.want {
-					t.Errorf("ERROR: got %v, want %v", got, c.want)
-				}
-				gotTitle := foundLesson.BookTitle()
-				if gotTitle != c.wantBookTitle {
-					t.Errorf("ERROR: got %q, want %q", gotTitle, c.wantBookTitle)
-				}
-			})
-		}
-	})
-}
-
-func compareLessonTitles[T content.Card](t *testing.T, got []books.Lesson[T], want []string) {
-	t.Helper()
-	gotTitles := []string{}
-	for _, l := range got {
-		gotTitles = append(gotTitles, l.Title())
+	testCases := []struct {
+		name    string
+		book    books.Book
+		wantLen int
+	}{
+		{
+			name:    "uninitialized",
+			wantLen: 0,
+		},
+		{
+			name:    "empty",
+			book:    books.New(book1, series1),
+			wantLen: 0,
+		},
+		{
+			name:    "one lesson",
+			book:    books.New(book1, series1, &lesson1),
+			wantLen: 1,
+		},
+		{
+			name:    "three lessons",
+			book:    books.New(book1, series1, &lesson1, &lesson2, &lesson3),
+			wantLen: 3,
+		},
+		{
+			name:    "empty book three lessons",
+			book:    emptyBook,
+			wantLen: 3,
+		},
+		{
+			name: "duplicate lessons",
+			book: books.New(book1, series1,
+				&lesson1, &lesson2, &lesson3,
+				&lesson1, &lesson2, &lesson3,
+			),
+			wantLen: 3,
+		},
 	}
 
-	if diff := cmp.Diff(gotTitles, want); diff != "" {
-		t.Errorf("ERROR: got- want+%s\n", diff)
+	for _, c := range testCases {
+		t.Run(c.name, func(t *testing.T) {
+			if got := len(c.book.Lessons()); got != c.wantLen {
+				t.Errorf("ERROR: got %v, want %v", got, c.wantLen)
+			}
+		})
 	}
 }
