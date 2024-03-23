d3b6b2f9750e64a730040e5f9ddd0213cb6c4323
jochen <jochen.czemmel@web.de>
2024-03-23 21:26:13 +0100

    move interfaces to package store

--- store/store.go
+++ store/store.go
@@ -2,6 +2,67 @@
 // kinds of application content.
 package store
 
-// const dbTypeJSON = "json"
+import (
+	"github.com/jochenczemmel/gobenkyoo/app/learn"
+	"github.com/jochenczemmel/gobenkyoo/content/books"
+	"github.com/jochenczemmel/gobenkyoo/content/kanjis"
+	"github.com/jochenczemmel/gobenkyoo/content/words"
+)
 
-// var DbTypes = []string{dbTypeJson}
+const DBTypeJSON = "json"
+
+var DbTypes = []string{dbTypeJson}
+
+// KanjiImporter defines the interface to import kanji cards.
+type KanjiImporter interface {
+	ImportKanji(string) ([]kanjis.Card, error)
+}
+
+// WordImporter defines the interface to import word cards.
+type WordImporter interface {
+	ImportWord(string) ([]words.Card, error)
+}
+
+// LibraryLoader defines the interface to load
+// kanji and word content.
+type LibraryLoader interface {
+	LoadLibrary(string) (books.Library, error)
+}
+
+// LibraryStorer defines the interface to store
+// kanji and word content.
+type LibraryStorer interface {
+	StoreLibrary(books.Library) error
+}
+
+// ClassroomLoader defines the interface to load learn boxes.
+type ClassroomLoader interface {
+	LoadClassroom(string) (learn.Classroom, error)
+}
+
+// ClassroomStorer defines the interface to store learn boxes.
+type ClassroomStorer interface {
+	StoreClassroom(learn.Classroom) error
+}
+
+// LibraryLoadStorer defines the interface to load and store
+// kanji and word content.
+type LibraryLoadStorer interface {
+	LibraryLoader
+	LibraryStorer
+}
+
+// LoadStorer defines the interface to load and store
+// word and kanji content and learn boxes.
+type LoadStorer interface {
+	LibraryLoader
+	LibraryStorer
+	ClassroomLoader
+	ClassroomStorer
+}
+
+// Loader can load learn boxes and kanji and word content.
+type Loader interface {
+	LibraryLoader
+	ClassroomLoader
+}
